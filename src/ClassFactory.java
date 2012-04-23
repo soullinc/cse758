91c73e3c12830efa6768139a572c99f64fc0cbdf
Kai Li <iverstyle@gmail.com>
2012-04-23 19:23:48 -0400

    refactored out a few method, add setMaxCls() in ClsFactory

--- src/ClassFactory.java
+++ src/ClassFactory.java
@@ -1,42 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClassFactory {
 
-	private static int totalClasses=0;
-	private static int totalMath=0;
-	private static int totalLA=0;
-	private static int totalRead=0;
-	
-	private ClassFactory(){
-		//empty
+	private static int totalClasses = 0;
+	private static int totalMath = 0;
+	private static int totalLA = 0;
+	private static int totalRead = 0;
+	private static int maxCls=15;
+
+	private ClassFactory() {
+		// empty
 	}
-	
-	public static Classes createClass(String name,int lvl){
+
+	static List<Classes> mathClsLst = new ArrayList<Classes>();
+	static List<Classes> laClsLst = new ArrayList<Classes>();
+	static List<Classes> readClsLst = new ArrayList<Classes>();
+
+	public static Classes createClass(String name, int lvl) {
 		totalClasses++;
-		if(name.equals("math")){
+		if (name.equals("math")) {
 			totalMath++;
-		}else if(name.equals("la")){
+		} else if (name.equals("la")) {
 			totalLA++;
-		}else if(name.equals("read")){
+		} else if (name.equals("read")) {
 			totalRead++;
-		}else{
-			System.err.println("Wrong class name passed into class factory:"+name);
+		} else {
+			System.err.println("Wrong class name passed into class factory:"
+					+ name);
 		}
-		return new Classes(name,lvl);
+		return new Classes(name, lvl);
 	}
-	
-	public static int getTotalClasses(){
+
+	public static int getTotalClasses() {
 		return totalClasses;
 	}
-	
-	public static int getTotalMath(){
+
+	public static int getTotalMath() {
 		return totalMath;
 	}
-	
-	public static int getTotalLA(){
+
+	public static int getTotalLA() {
 		return totalLA;
 	}
-	
-	public static int getTotalRead(){
+
+	public static int getTotalRead() {
 		return totalRead;
+	}
+
+	/**
+	 * Check if a student can be assigned to a particular class
+	 * 
+	 * @param std
+	 * @param cls
+	 * @return
+	 * 			True if yes, false otherwise.
+	 */
+	public static boolean compatible(Students std, Classes cls) {
+		if (cls.getClsName().equals("math")) {
+			if(cls.getLvl()==std.getMath() && Math.abs(cls.getLowestAge()-std.getAge())<3 && cls.getTotal()<5){
+				return true;
+			}else{
+				return false;
+			}
+		} else if (cls.getClsName().equals("la")) {
+			if(cls.getLvl()==std.getLA() && Math.abs(cls.getLowestAge()-std.getAge())<3 && cls.getTotal()<5){
+				return true;
+			}else{
+					return false;
+				}
+		} else if (cls.getClsName().equals("read")) {
+			if(cls.getLvl()==std.getRead() && Math.abs(cls.getLowestAge()-std.getAge())<3 && cls.getTotal()<5){
+				return true;
+			}else{
+				return false;
+			}
+		} else {
+			System.err
+					.println("A Classes object passed into ClassFactory.compatible method is labeled wrong class name");
+			return false;
+		}
+	}
+	
+	public static void setMaxCls(int i){
+		maxCls=i;
 	}
 }
--- src/Schedulizer.java
+++ src/Schedulizer.java
@@ -13,108 +13,102 @@ public class Schedulizer {
 
 		List<Students> std = stds.getStudents();
 
-		/*Scanner in = new Scanner(System.in);
+		/*
+		 * Scanner in = new Scanner(System.in);
+		 * 
+		 * System.out.print("Input student info: name:"); String name =
+		 * in.nextLine();
+		 * 
+		 * while (!name.equals("-1")) {
+		 * 
+		 * System.out.print("                    age: "); int age =
+		 * in.nextInt(); System.out.print("                    Math level: ");
+		 * int math = in.nextInt();
+		 * System.out.print("                    LA level: "); int la =
+		 * in.nextInt();
+		 * System.out.print("                    Reading level: "); int read =
+		 * in.nextInt(); std.add(new Students(name, age, math, la, read));
+		 * in.nextLine(); System.out.print("Input student info: name:"); name =
+		 * in.nextLine(); }
+		 * 
+		 * System.out.println("\nTotal students: " + std.size());
+		 */
 
-		System.out.print("Input student info: name:");
-		String name = in.nextLine();
-
-		while (!name.equals("-1")) {
-
-			System.out.print("                    age: ");
-			int age = in.nextInt();
-			System.out.print("                    Math level: ");
-			int math = in.nextInt();
-			System.out.print("                    LA level: ");
-			int la = in.nextInt();
-			System.out.print("                    Reading level: ");
-			int read = in.nextInt();
-			std.add(new Students(name, age, math, la, read));
-			in.nextLine();
-			System.out.print("Input student info: name:");
-			name = in.nextLine();
-		}
-
-		System.out.println("\nTotal students: " + std.size());*/
-
-		//sort by age.
+		// sort by age.
 		Collections.sort(std);
 
-		//print sorted list.
-		for(int i=0;i<std.size();i++){
-		 System.out.println(std.get(i).toString());
-		 }
-
-		List<Classes> math = new ArrayList<Classes>();
-		List<Classes> la = new ArrayList<Classes>();
-		List<Classes> read = new ArrayList<Classes>();
-
-		Classes mathClass=null,laClass=null,readClass=null;
+		// print sorted list.can be deleted for final product
 		for (int i = 0; i < std.size(); i++) {
-	
-			int age = std.get(i).getAge();
-			int mathLvl = std.get(i).getMath();
-			int laLvl=std.get(i).getLA();
-			int readLvl=std.get(i).getRead();
-			
-			//schedulize math class
-			for(Classes cls:math){
-				if(cls.getLvl()==mathLvl && Math.abs(cls.getLowestAge()-age)<3 && cls.getTotal()<5){
-					mathClass=cls;
+			System.out.println(std.get(i).toString());
+		}
+
+		// List<Classes> math = new ArrayList<Classes>();
+		// List<Classes> la = new ArrayList<Classes>();
+		// List<Classes> read = new ArrayList<Classes>();
+
+		Classes mathClass = null, laClass = null, readClass = null;
+		for (int i = 0; i < std.size(); i++) {
+
+			// schedulize math class
+			for (Classes cls : ClassFactory.mathClsLst) {
+				if (ClassFactory.compatible(std.get(i), cls)) {
+					mathClass = cls;
+					break;
 				}
 			}
-			if(mathClass==null){
-				mathClass=ClassFactory.createClass("math", mathLvl);
-				math.add(mathClass);
+			if (mathClass == null) {
+				mathClass = ClassFactory.createClass("math", std.get(i)
+						.getMath());
+				ClassFactory.mathClsLst.add(mathClass);
 			}
 			mathClass.addStd(std.get(i));
-			mathClass=null;
-			
-			
-			//schedulize LA class
-			for(Classes cls:la){
-				if(cls.getLvl()==laLvl && Math.abs(cls.getLowestAge()-age)<3 && cls.getTotal()<5){
-					laClass=cls;
+			mathClass = null;
+
+			// schedulize LA class
+			for (Classes cls : ClassFactory.laClsLst) {
+				if (ClassFactory.compatible(std.get(i), cls)) {
+					laClass = cls;
 				}
 			}
-			if(laClass==null){
-				laClass=ClassFactory.createClass("la", laLvl);
-				la.add(laClass);
+			if (laClass == null) {
+				laClass = ClassFactory.createClass("la", std.get(i).getLA());
+				ClassFactory.laClsLst.add(laClass);
 			}
 			laClass.addStd(std.get(i));
-			laClass=null;
-			
-			//schedulize reading class
-			for(Classes cls:read){
-				if(cls.getLvl()==readLvl && Math.abs(cls.getLowestAge()-age)<3 && cls.getTotal()<5){
-					readClass=cls;
+			laClass = null;
+
+			// schedulize reading class
+			for (Classes cls : ClassFactory.readClsLst) {
+				if (ClassFactory.compatible(std.get(i), cls)) {
+					readClass = cls;
 				}
 			}
-			if(readClass==null){
-				readClass=ClassFactory.createClass("read", readLvl);
-				read.add(readClass);
+			if (readClass == null) {
+				readClass = ClassFactory.createClass("read", std.get(i)
+						.getRead());
+				ClassFactory.readClsLst.add(readClass);
 			}
 			readClass.addStd(std.get(i));
-			readClass=null;
-			
-			
-			
-			
-			
+			readClass = null;
+
 		}
-		
+
 		System.out.println("*********Results************");
-		System.out.println("There are "+math.size()+" classes for math:");
-		for(Classes cls: math){
+		System.out.println("There are " + ClassFactory.mathClsLst.size()
+				+ " classes for math:");
+		for (Classes cls : ClassFactory.mathClsLst) {
 			System.out.println(cls.toString());
 		}
-		
-		System.out.println("\nThere are "+la.size()+" classes for LA:");
-		for(Classes cls: la){
+
+		System.out.println("\nThere are " + ClassFactory.laClsLst.size()
+				+ " classes for LA:");
+		for (Classes cls : ClassFactory.laClsLst) {
 			System.out.println(cls.toString());
 		}
-		
-		System.out.println("\nThere are "+read.size()+" classes for reading:");
-		for(Classes cls: read){
+
+		System.out.println("\nThere are " + ClassFactory.readClsLst.size()
+				+ " classes for reading:");
+		for (Classes cls : ClassFactory.readClsLst) {
 			System.out.println(cls.toString());
 		}
 
