1d5b516224b6874f8ef4d7c9bc3225cef3010d73
Kai Li <iverstyle@gmail.com>
2012-04-29 05:36:37 -0400

    schedulizer added a runTest() method that will automate testing process on verifying the generated schedule, unfinished

--- src/Schedulizer.java
+++ src/Schedulizer.java
@@ -44,126 +44,128 @@ public class Schedulizer {
 		List<Students> unluckyStd = new ArrayList<Students>();
 		boolean foundCls;
 		for (int i = 0; i < std.size(); i++) {
-			
-			foundCls=false;
-				
+
+			foundCls = false;
+
 			// schedulize math class
-			//check if there is already a class compatible with current student
+			// check if there is already a class compatible with current student
 			for (Classes cls : ClassFactory.mathClsLst) {
 				if (ClassFactory.compatible(std.get(i), cls)) {
 					mathClass = cls;
 					mathClass.addStd(std.get(i));
-					foundCls=true;
+					foundCls = true;
 					break;
 				}
 			}
-			
-			//no compatible cls, check if we can create a new class, remember there is a limit on max#ofCls
-//			if(mathClass == null && ClassFactory.getMaxCls()<=ClassFactory.getTotalMath()){
-//				unluckyStd.add(std.get(i));
-//				
-//				continue;// those unlucky students...
-//			}else if (mathClass == null) {
-//				mathClass = ClassFactory.createClass("math", std.get(i)
-//						.getMath());
-//				ClassFactory.mathClsLst.add(mathClass);
-//				mathClass.addStd(std.get(i));
-//				
-//			}
-			
-			if(!foundCls){
-				if(ClassFactory.getTotalMath()<ClassFactory.getMaxCls()){
+
+			// no compatible cls, check if we can create a new class, remember
+			// there is a limit on max#ofCls
+			// if(mathClass == null &&
+			// ClassFactory.getMaxCls()<=ClassFactory.getTotalMath()){
+			// unluckyStd.add(std.get(i));
+			//
+			// continue;// those unlucky students...
+			// }else if (mathClass == null) {
+			// mathClass = ClassFactory.createClass("math", std.get(i)
+			// .getMath());
+			// ClassFactory.mathClsLst.add(mathClass);
+			// mathClass.addStd(std.get(i));
+			//
+			// }
+
+			if (!foundCls) {
+				if (ClassFactory.getTotalMath() < ClassFactory.getMaxCls()) {
 					mathClass = ClassFactory.createClass("math", std.get(i)
 							.getMath());
 					ClassFactory.mathClsLst.add(mathClass);
 					mathClass.addStd(std.get(i));
-					foundCls=true;
-				}else{
+					foundCls = true;
+				} else {
 					unluckyStd.add(std.get(i));
 					continue;// those unlucky students...
 				}
 			}
-			
-			
-			foundCls=false;
+
+			foundCls = false;
 			// schedulize LA class
 			for (Classes cls : ClassFactory.laClsLst) {
 				if (ClassFactory.compatible(std.get(i), cls)) {
 					laClass = cls;
 					laClass.addStd(std.get(i));
-					foundCls=true;
+					foundCls = true;
 					break;
 				}
 			}
-//			if(laClass == null && ClassFactory.getMaxCls()<=ClassFactory.getTotalLA()){
-//				unluckyStd.add(std.get(i));
-//				//roll back...
-//				mathClass.removeStd(std.get(i).getName());
-//				continue;// those unlucky students...
-//			}else if (laClass == null) {
-//				laClass = ClassFactory.createClass("la", std.get(i).getLA());
-//				ClassFactory.laClsLst.add(laClass);
-//				laClass.addStd(std.get(i));
-//			}
-			if(!foundCls){
-				if(ClassFactory.getTotalLA()<ClassFactory.getMaxCls()){
-					laClass = ClassFactory.createClass("la", std.get(i).getLA());
+			// if(laClass == null &&
+			// ClassFactory.getMaxCls()<=ClassFactory.getTotalLA()){
+			// unluckyStd.add(std.get(i));
+			// //roll back...
+			// mathClass.removeStd(std.get(i).getName());
+			// continue;// those unlucky students...
+			// }else if (laClass == null) {
+			// laClass = ClassFactory.createClass("la", std.get(i).getLA());
+			// ClassFactory.laClsLst.add(laClass);
+			// laClass.addStd(std.get(i));
+			// }
+			if (!foundCls) {
+				if (ClassFactory.getTotalLA() < ClassFactory.getMaxCls()) {
+					laClass = ClassFactory
+							.createClass("la", std.get(i).getLA());
 					ClassFactory.laClsLst.add(laClass);
 					laClass.addStd(std.get(i));
-					foundCls=true;
-				}else{
+					foundCls = true;
+				} else {
 					unluckyStd.add(std.get(i));
-					//roll back...
+					// roll back...
 					mathClass.removeStd(std.get(i).getId());
 					continue;// those unlucky students...
 				}
 			}
-			
-			
-			
-			foundCls=false;
+
+			foundCls = false;
 			// schedulize reading class
 			for (Classes cls : ClassFactory.readClsLst) {
 				if (ClassFactory.compatible(std.get(i), cls)) {
 					readClass = cls;
 					readClass.addStd(std.get(i));
-					foundCls=true;
+					foundCls = true;
 					break;
 				}
 			}
-//			if(readClass == null && ClassFactory.getMaxCls()<=ClassFactory.getTotalRead()){
-//				unluckyStd.add(std.get(i));
-//				//roll back...
-//				mathClass.removeStd(std.get(i).getName());
-//				laClass.removeStd(std.get(i).getName());
-//				continue;// those unlucky students...
-//			}else if (readClass == null) {
-//				readClass = ClassFactory.createClass("read", std.get(i)
-//						.getRead());
-//				ClassFactory.readClsLst.add(readClass);
-//				readClass.addStd(std.get(i));
-//			}
-			
-			if(!foundCls){
-				if(ClassFactory.getTotalRead()<ClassFactory.getMaxCls()){
-					readClass = ClassFactory.createClass("read", std.get(i).getRead());
+			// if(readClass == null &&
+			// ClassFactory.getMaxCls()<=ClassFactory.getTotalRead()){
+			// unluckyStd.add(std.get(i));
+			// //roll back...
+			// mathClass.removeStd(std.get(i).getName());
+			// laClass.removeStd(std.get(i).getName());
+			// continue;// those unlucky students...
+			// }else if (readClass == null) {
+			// readClass = ClassFactory.createClass("read", std.get(i)
+			// .getRead());
+			// ClassFactory.readClsLst.add(readClass);
+			// readClass.addStd(std.get(i));
+			// }
+
+			if (!foundCls) {
+				if (ClassFactory.getTotalRead() < ClassFactory.getMaxCls()) {
+					readClass = ClassFactory.createClass("read", std.get(i)
+							.getRead());
 					ClassFactory.readClsLst.add(readClass);
 					readClass.addStd(std.get(i));
-					foundCls=true;
-				}else{
+					foundCls = true;
+				} else {
 					unluckyStd.add(std.get(i));
-					//roll back...
+					// roll back...
 					mathClass.removeStd(std.get(i).getId());
 					laClass.removeStd(std.get(i).getId());
 					continue;// those unlucky students...
 				}
 			}
-			
-			
-//			//get ready for next round.
-//			mathClass=null;
-//			laClass=null;
-//			readClass = null;
+
+			// //get ready for next round.
+			// mathClass=null;
+			// laClass=null;
+			// readClass = null;
 		}
 
 		System.out.println("*********Results************");
@@ -185,11 +187,90 @@ public class Schedulizer {
 			System.out.println(cls.toString());
 		}
 
-		
 		System.out.println("*******Unlucky Students********");
-		for(Students stdss:unluckyStd){
+		for (Students stdss : unluckyStd) {
 			System.out.println(stdss.toString());
 		}
+
+	}
+
+	public void runTest() {
+		System.out
+				.println("Running automatic testing for generated schedules:");
+
+		// ==============================
+		System.out
+				.println("Testing on restriction on total number of classes:");
+		System.out.println("	Max # of classes: " + ClassFactory.getMaxCls());
+		System.out.println("	# of all math classes: "
+				+ ClassFactory.getTotalMath());
+		System.out.println("	# of all LArt classes: "
+				+ ClassFactory.getTotalLA());
+		System.out.println("	# of all reading classes: "
+				+ ClassFactory.getTotalRead());
+		boolean result = ClassFactory.getTotalMath() <= ClassFactory
+				.getMaxCls()
+				&& ClassFactory.getTotalLA() <= ClassFactory.getMaxCls()
+				&& ClassFactory.getTotalRead() <= ClassFactory.getMaxCls();
+
+		if (result) {
+			System.out.println("	Result: Pass");
+		} else {
+			System.out.println("	Result: Fail");
+			System.exit(0);
+		}
+		// ==============================
+		System.out
+				.println("Testing on restriction on max number of student per class:");
+		System.out.println("	Max # of students per class: 5");
+		result = true;
+		for (Classes cls : ClassFactory.mathClsLst) {
+			if (cls.getTotal() > 5) {
+				result = false;
+				break;
+			}
+		}
+		if (result) {
+			System.out.println("\tAll math classes passed.");
+		} else {
+			System.out.println("\tAt least one math class failed");
+			System.exit(0);
+		}
+		for (Classes cls : ClassFactory.laClsLst) {
+			if (cls.getTotal() > 5) {
+				result = false;
+				break;
+			}
+		}
+		if (result) {
+			System.out.println("\tAll LA classes passed.");
+		} else {
+			System.out.println("\tAt least one LA class failed");
+			System.exit(0);
+		}
+
+		for (Classes cls : ClassFactory.readClsLst) {
+			if (cls.getTotal() > 5) {
+				result = false;
+				break;
+			}
+		}
+		if (result) {
+			System.out.println("\tAll reading classes passed.");
+		} else {
+			System.out.println("\tAt least one reading class failed");
+			System.exit(0);
+		}
+		//========================
+		System.out.println("Testing on age restriction:");
+		//========================
+		System.out.println("Testing on behavior level restriction:");
+		//========================
+		System.out.println("Testing on whether each class has only one level of students:");
+		//=======================
+		System.out.println("Testing on unlucky students are truly unlucky, and cannot be assgined to any class:");
+		//======================
+		
 	}
 
 }
